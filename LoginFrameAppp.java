77434033b6e90900bf54034c51bf3235aa51b7ba
Mira123mimi <mphomiranda61@gmail.com>
2025-08-25 01:04:40 +0200

    Add files via upload

---
+++ LoginFrameAppp.java
@@ -0,0 +1,14 @@
+
+package loginframeappp;
+
+import za.ac.tut.ui.LoginFrame;
+
+
+public class LoginFrameAppp {
+
+    public static void main(String[] args) 
+    {
+       new LoginFrame();
+    }
+    
+}
